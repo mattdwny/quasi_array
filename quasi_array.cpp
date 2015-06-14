1d1156e309ffe9e0e00b15868807085c6eeafb89
Matt Downey <matthew.r.downey@gmail.com>
2015-06-14 07:24:11 -0400

    Everything is still broken

--- quasi_array.cpp
+++ quasi_array.cpp
@@ -1,28 +1,73 @@
+#include "quasi_array.h"
+
+
+
 template <typename T>
 quasi_array<T>::quasi_array(size_t minSize = 1)
 {	
-	size = startSize;
-	cacheBlockSize = blockSize;
+	lookupTable = (T**) malloc(sizeof(T*));
+	nearArray = farArray = lookupTable[0];
+	
+	size = 0;
 	tableSize = 1;
-	lookupTable = (T**) malloc(tableSize * sizeof(T*));
+	capacity = chunkSize = minSize;
+
+	maxBit = -1;
 }
 
-//deceivingly O(1)
 template <typename T>
-size_t quasi_array<T>::superindexOf(size_t i, size_t maxBit = sizeof(size_t)*8-1)
-{
-	size_t iter = maxBit;
-	while(iter)
+quasi_array<T>::~quasi_array()
+{	
+	for(size_t i = 0; i < tableSize; ++i)
+		free lookupTable[i];
+	
+	free lookupTable;
+}
+
+template <typename T>
+quasi_array<T>::push_back(size_t minSize = 1)
+{	
+	if(size == capcity)
 	{
-		if(i >> iter) return iter;
+
 	}
-	if(iter) return 1;
+	size_t
+	farArray[]
+	++size;
+}
+
+template <typename T>
+quasi_array<T>::pop_back(size_t minSize = 1)
+{	
+	--size;
+
+}
+
+/**
+ * deceivingly O(1): probability of finding an element is at best sum ( 1*(1/2) + 2*(1/4) + 3*(1/8) + ... + i*(2^-i) )/n, so 2
+ */
+template <typename T>
+size_t quasi_array<T>::superindexOf(size_t i)
+{
+	size_t real_i = i/chunkSize;
+	size_t iter = maxBit;
+	while(iter > 0)
+	{
+		if(real_i >> iter) return iter;
+	}
+	if(real_i) return 1;
 	return 0;
+}
+
+template <typename T>
+size_t quasi_array<T>::subindexOf(size_t i)
+{
+	return subindexOf(i, superindexOf(i));
 }
 
 template <typename T>
 size_t quasi_array<T>::subindexOf(size_t i, size_t superindex)
 {
-	return i >> superindex;
+	return (i/chunkSize) & ~superindex;
 }
 
--- quasi_array.h
+++ quasi_array.h
@@ -2,38 +2,50 @@
  * Quasi arrays are non-contiguous memory storage containers that uses a look-up table that points to built-in arrays of increasing size.
  * Given arrays of size 1, 1, 2, 4,  8, 16 ... 2^(i) the sum is:
  *						1, 2, 4, 8, 16, 32 ... 2^(i+1) which is a trick used to index into a quasi array.
- * important to define are the superindex: the position of the most significant (1) bit or -1 if it does not exist, and
- *						   the subindex: the desired position in the array shifted right by the superindex
+ * important to define are the superindex(i): the position of the most significant (1) bit in index i or -1 if it does not exist, and
+ *						   the subindex(i): i & ~superindex
  * Indexing into an array will take on average O(1) time (Worst case is O(log(n)). Auxiliary space requirement is O(n + lg(n)) due to unused array allocation space and pointers respectively.
- * Practically, quarrays are intended to mitigate the side effects of reallocating arrays during resize operations, which makes access time more reliable during runtime applications.
+ * Practically, quasi arrays are intended to mitigate the side effects of reallocating arrays during resize operations, which makes access time more reliable during runtime applications.
  * Accessing all elements with an iterator includes traversing lg(n) sets of data that have a size of n elements. This means that traversal with an iterator is O(n + lg(n)), so O(n).
  * if iterator traversals happen frequently (>= O(n)/lg(n) times) before arrayification or destruction then the benefit of using quasi arrays is strictly due to runtime guarantees.
  * @author Matthew Russell Downey (MattDwny)
  */
 
 template <typename T>
-class quarray
+class quasi_array
 {
 private:
 	T** lookupTable; //an array of pointers to contiguous arrays of object T. lookupTable[0][0] will always return the first element. The array sizes will be cacheBlockSize, cacheBlockSize, 2*cacheBlockSize, 4*cacheBlockSize, 8*cacheBlockSize
 	T*  nearArray; //stack optimization
 	T*  farArray; //
 
-	size_t chunkSize;
 	size_t size;
 	size_t capacity;
+	size_t chunkSize;
 	size_t tableSize;
+
+	size_t maxBit;
+
+	size_t superindexOf(size_t i);
+	size_t subindexOf(size_t i);
 
 public:
 	/**
 	 * constructors
 	 */
+	quasi_array();
 	quasi_array(size_t minSize);
+
+	/**
+	 * destructors
+	 */
+	~quasi_array();
 
 	/**
 	 *  element access
 	 */
-	T at(size_t t);
+	T at(size_t i);
+	T at(size_t superindex, size_t subindex);
 	T operator[]();
 
 	/**
@@ -60,7 +72,7 @@ public:
 	void reserve();
 	size_t capacity();
 	void shrink_to_fit();
-	void unshuffle();
+	void arrayify();
 
 	/**
 	 * modifiers
